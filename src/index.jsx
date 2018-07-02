9d7b8cd43d4152a309a143f99e487721ec6efde8
Pete Nelson <nelsonp@aetna.com>
2018-07-01 21:49:26 -0600

    Remove root

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.scss'
-import Root from './root'
-
+import { Provider } from 'react-redux'
+import { ConnectedRouter as Router } from 'react-router-redux'
+import store, { history } from 'store'
+import { PublicRoutes } from './routes'
 import './polyfills'
 
+import './styles/index.scss'
+
 ReactDOM.render(
-  <Root />,
+  <Provider store={store}>
+    <Router history={history}>
+      <PublicRoutes />
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 )
