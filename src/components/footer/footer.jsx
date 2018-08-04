850f11406a9042aac5f001fc13bacea02c0a1d28
Pete Nelson <nelsonp@aetna.com>
2018-08-04 13:22:02 -0600

    Fix mobile position for full screen

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,7 +5,7 @@ import './styles.scss'
 const Footer = () => {
   // const year = 2018
 
-  return (<div className="sp-footer">SPACECAKE PRODUCTIONS.</div>)
+  return (<div className="sp-footer">SPACECAKE PRODUCTIONS</div>)
 }
 
 export default Footer
